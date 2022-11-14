d27afd75971272188ee55fe3003cf193bac326c7
huzaifareality <huzaifacspucit@gmail.com>
2022-11-13 20:09:22 -0800

    questions class

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.quizapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView totalQ, question;
+    Button b1, b2, b3, b4, b5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);totalQ = findViewById(R.id.textView);
+        question = findViewById(R.id.textView2);
+        b1 = findViewById(R.id.button);
+        b2 = findViewById(R.id.button2)
+        b3 = findViewById(R.id.button3;
+        b4 = findViewById(R.id.button4;
+        b5 = findViewById(R.id.button5);
     }
+
 }
+
+
+
---
+++ app/src/main/java/com/example/quizapp/questions.java
@@ -0,0 +1,25 @@
+package com.example.quizapp;
+
+public class questions {
+
+    public static string question[] =
+    {
+            "What is the software in option?",
+            "What is not a social media app?",
+            "Which is not a high level programming language?"
+    };
+
+    public static string options[][] =
+    {
+            {"monitor", "Android Studio", "mouse", "speaker"},
+            {"Minecraft", "instagram", "twitter", "facebook"},
+            {"kotlin", "java", "html", "python"}
+    };
+
+    public static string correctAns[] =
+    {
+            "Android Studio",
+            "Minecraft",
+            "html"
+    };
+}
